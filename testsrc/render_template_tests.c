f47e43be093e45dbbb05b923e4f09bc54672956f
James Cox-Morton <th3james@fastmail.fm>
2016-07-13 08:49:25 +0100

    tart on test for basic template rendering

--- testsrc/render_template_tests.c
+++ testsrc/render_template_tests.c
@@ -1,7 +1,23 @@
-#include <string.h>
 #include <stdio.h>
 #include <assert.h>
+#include "../read_replacement_args.c"
+
+void testRenderBasicTemplate() {
+  int argc = 2;
+  char *argv[] = {"name:", "World"};
+
+  DictEntry_t entries[calculateDictEntryCount(argc)];
+  Dict_t dict = readDict(argc, argv, entries);
+
+  static const char *templateFileName = "template.hsh";
+  FILE *templateFile;
+  if ((templateFile = fopen(templateFileName, "r")) == NULL) {
+    printf("Failed to test template file open file %s\n", templateFileName);
+  } else {
+    printf("Opened file: %s\n", argv[1]);
+  }
+}
 
 void testSuiteRenderTemplate() {
-  assert(1 == 1);
+  testRenderBasicTemplate();
 }
